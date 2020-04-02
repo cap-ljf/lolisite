6b6597b6c5a6516c57a3f01ce38d36ca98f763c6
cap-ljf <1579461369@qq.com>
2020-04-02 22:54:54 +0800

    init

---
+++ src/main/java/com/lolisite/lolisite/LolisiteApplication.java
@@ -0,0 +1,13 @@
+package com.lolisite.lolisite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolisiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolisiteApplication.class, args);
+	}
+
+}
